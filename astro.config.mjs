c750c5bf835ee2b6aaa5325e73a2032da9892419
Bryan Taylor <Bryan@sp33dsk8r.com>
2026-06-22 21:42:26 -0500

    Fix Tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://sp33dsk8r.com',
-  trailingSlash: 'never'
-});
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        speedBlack: '#0a0a0a',
+        speedYellow: '#ffd700',
+        speedBlue: '#002868',
+        speedRed: '#bf0a30',
+      }
+    }
+  }
+}
